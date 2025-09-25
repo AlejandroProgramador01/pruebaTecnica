c9d455da38070963a7f0e1643e53c9ef9a381cff
AlejandroProgramador01 <alejandrotrujillo.personalmail.com>
2025-09-24 19:57:00 -0500

    subiendo mi proyecto

---
+++ src/test/java/com/crud/pruebaTecnica/PruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.pruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
